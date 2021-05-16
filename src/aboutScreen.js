5b80dc8bba0ab230b0fcd9d9dd8332ca0c9f0d98
ai-amarismic <66122965+ai-amarismic@users.noreply.github.com>
2021-05-16 14:11:42 +0200

    Added About and Contact Screens

--- src/aboutScreen.js
+++ src/aboutScreen.js
@@ -1,6 +1,9 @@
 import SearchAndCategory from './searchAndCategory.js'
 import HomeDiv from './homeDiv.js'
 import React, { useEffect, useState, useContext } from "react";
+import { Divider } from '@material-ui/core';
+import "./aboutScreen.css"
+import logo from './imgs/logo.jpeg'
 
 
 
@@ -11,28 +14,38 @@ import React, { useEffect, useState, useContext } from "react";
 
 export default function AboutScreen() {
 
-    
+
     const [loggedUser, setLoggedUser] = useState('amar')
 
-    
 
-  
+
+
 
     const handleSetLoggedUser = (user) => {
         setLoggedUser(user);
     }
 
- 
-  
 
-    
+
+
+
     return (
-        <>
-            <SearchAndCategory   handleSetLoggedUser={handleSetLoggedUser} />
-            <HomeDiv passedName='Siiiidje' />
-            {/* 
-            P.S. probaj u Cars tabu filtere sa strane sto smo pravili, povezo sam ih sinoc
-             */}
-        </>
+         <>
+        <SearchAndCategory handleSetLoggedUser={handleSetLoggedUser}/>
+         <HomeDiv passedName='About Us' />
+        <div id='aboutUsText' >
+            <h2 > About Cars.ba </h2>
+             <p >
+                    Cars.ba is an online marketplace
+                    for car lovers, dealers, buyers, and sellers.Network that helps you to find the best price on a new vehicle, as well as a reliable mechanic and dealer.The core idea behind the system is a platform just
+                    for buying and selling cars.We have a wide variety of component stores in one location
+        for DIY enthusiasts.This is a website that meet strict guidelines in order to maintain its simplicity and modern appearance.It include automatically created article names, a price that must be specified, and no article promotions. < br />
+        Our main aim is to bring together people who are passionate about cars and everything related to them, and to make them happy every time they visit <i> Cars.ba. </i> 
+        </p> 
+        </div>
+
+        
+    </>
     )
+
 }
--- src/contactScreen.js
+++ src/contactScreen.js
@@ -1,6 +1,7 @@
 import SearchAndCategory from './searchAndCategory.js'
 import HomeDiv from './homeDiv.js'
 import React, { useEffect, useState, useContext } from "react";
+import "./contactScreen.css"
 
 
 
@@ -11,29 +12,62 @@ import React, { useEffect, useState, useContext } from "react";
 
 export default function ContactScreen() {
 
-    
+
+
+
+
     const [loggedUser, setLoggedUser] = useState('amar')
-    
-    
-    
 
-    
 
-  
+
+
+
+
+
 
     const handleSetLoggedUser = (user) => {
         setLoggedUser(user);
     }
 
- 
-  
-
-    
-    return (
+    return ( 
         <>
-            <SearchAndCategory  handleSetLoggedUser={handleSetLoggedUser} />
-            <HomeDiv passedName='Siiiiidje' />
-            
+        <SearchAndCategory handleSetLoggedUser = { handleSetLoggedUser }/> 
+        <HomeDiv passedName = 'Contact Us' />
+
+        <div class = "formContainer" >
+        <form >
+        <span >
+        <input type = "text"
+        id = "inputBox"
+        placeholder = "Full Name" />
+        </span> 
+        <span id = "span" >
+        <input id = "inputBox"
+        type = "number"
+        placeholder = "Phone" />
+        </span>
+
+        <span >
+        <input type = "email"
+        id = "inputBox"
+        placeholder = "Email" />
+        </span>
+
+        <br />
+        <h3 > Enter your message: </h3> 
+        <input className = "messageBox"
+        type = "text"
+        placeholder = "" />
+        <br />
+        <div>
+        <input type = "button"
+        name = "submit"
+        id = "submitButton"
+        value = "Submit"
+        onclick = "signup(); return false;" />
+        </div> 
+        </form> 
+        </div>  
         </>
     )
 }
